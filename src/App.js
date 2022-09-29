fc4aa6ac89ce2723c5d694540b5ba9741d19256a
arfan36 <arfanur36@gmail.com>
2022-09-29 17:28:47 +0600

    added favicon, bootstrap card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import ExerciseDetails from './components/ExerciseDetails/ExerciseDetails';
+import ExerciseItems from './components/ExerciseItems/ExerciseItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='full-container'>
+      <div className='exercise-items'>
+        <ExerciseItems></ExerciseItems>
+      </div>
+      <div className='exercise-details'>
+        <ExerciseDetails></ExerciseDetails>
+      </div>
     </div>
   );
 }
---
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -0,0 +1,14 @@
+import './ExerciseDetails.css'
+import React from 'react';
+
+const ExerciseDetails = () => {
+    return (
+        <div>
+            <div className='owner-info'>
+                <h3>Arfanur Rahman</h3>
+            </div>
+        </div>
+    );
+};
+
+export default ExerciseDetails;
